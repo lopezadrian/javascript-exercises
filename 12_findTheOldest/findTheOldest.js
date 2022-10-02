115d638cf7f15f7b6e60e05c98b3a6c11ef1fa58
Adrian Lopez <adallobi@gmail.com>
2022-10-02 14:24:03 -0400

    Completed 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+function calculateAge(birthYear, deathYear) {
+    let age;
+    if (!deathYear) {
+        let currentYear = new Date();
+        currentYear = currentYear.getFullYear();
+        age = currentYear - birthYear;
+        return age;
+    }
+    age = deathYear - birthYear;
+    return age;
+}
 
+const findTheOldest = function(array) {
+    const oldestFound = array.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = calculateAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const currentAge = calculateAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldestPerson;
+    })
+    return oldestFound;
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+           
