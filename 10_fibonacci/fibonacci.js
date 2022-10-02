facc11bbc0b7203e6363b9d2a6ebadade6805b9e
Adrian Lopez <adallobi@gmail.com>
2022-10-01 21:32:23 -0400

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    const nthNumber = parseInt(number);
+    if (nthNumber < 0) return "OOPS";
+    let currentNumber = 1;
+    let previousNumber = 1;
+    let nextNumber;
+    for (i = 0; i < nthNumber - 2; i++) {
+        nextNumber = currentNumber + previousNumber;
+        previousNumber = currentNumber;
+        currentNumber = nextNumber;
+    }
+    return currentNumber;
 };
 
 // Do not edit below this line
